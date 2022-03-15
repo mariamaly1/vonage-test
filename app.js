19323e66962d0d022356dc27b661d049c0df1a07
Mariam Aly <mariamaly@mariams-mbp.lan>
2022-03-15 19:14:26 -0400

    Simplifying the code by moving the boolean flag inside the function

--- app.js
+++ app.js
@@ -2,19 +2,18 @@
 var apiKey = "47465591";
 var sessionId = "2_MX40NzQ2NTU5MX5-MTY0NzM2ODQyNjk5Mn53RThsc2R2VnBCd2xQWXhGdEF2TUhJc1B-fg";
 var token = "T1==cGFydG5lcl9pZD00NzQ2NTU5MSZzaWc9NmUyYjk0MjlhOGM4MDQzNGZmMGZmNzBlZTJhZWM0M2E4YmY2NzZmYjpzZXNzaW9uX2lkPTJfTVg0ME56UTJOVFU1TVg1LU1UWTBOek0yT0RReU5qazVNbjUzUlRoc2MyUjJWbkJDZDJ4UVdYaEdkRUYyVFVoSmMxQi1mZyZjcmVhdGVfdGltZT0xNjQ3MzY4NDU4Jm5vbmNlPTAuODU0NTM4NTgyMzMwOTcxNSZyb2xlPW1vZGVyYXRvciZleHBpcmVfdGltZT0xNjQ5OTYwNDU4JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9";
-var flag = true;
 // (optional) add server code here
 var publisher = null;
 
 initializeSession();
 
 function disableEnableVideoAndAudio () {
+  var flag = true;
   var button = document.getElementById("button");
   if (button.value.includes("Disable")) {
     flag = false;
     button.value = "Enable Video and Audio"
   } else {
-    flag = true;
     button.value = "Disable Video and Audio"
   }
   publisher.publishVideo(flag);
